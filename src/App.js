b03efbf5668958111a8593bd02e03325e64bc180
Kristina <crsbka@bk.ru>
2022-03-15 23:04:25 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo-50.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi
+        </p>
+
+      </header>
+    </div>
+  );
+}
