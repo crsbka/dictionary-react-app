01c11d0e65670a10edb18af18b8f5c93f4c1094d
Kristina <crsbka@bk.ru>
2022-03-28 19:22:57 -0400

    Revised code

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
     return (
         <div className="Phonetic">
             <a href={props.phonetic.audio} target="_blank">
-                Listen
+                Click to listen
             </a>
             <br/>
             {props.phonetic.text}
